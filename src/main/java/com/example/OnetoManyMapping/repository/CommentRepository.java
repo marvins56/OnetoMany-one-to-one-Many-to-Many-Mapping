977ca7415b87af6a89cfe429c93c742277ab5651
Kauta Marvin <82571414+marvins56@users.noreply.github.com>
2023-05-16 21:27:12 +0300

    update comment repo
    method that retrieves comments for a specific post in a pageable format

--- src/main/java/com/example/OnetoManyMapping/repository/CommentRepository.java
+++ src/main/java/com/example/OnetoManyMapping/repository/CommentRepository.java
@@ -1,5 +1,9 @@
 package com.example.OnetoManyMapping.repository;
 
+import java.awt.print.Pageable;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +11,6 @@ import com.example.OnetoManyMapping.model.Comment;
 
 @Repository
 public interface CommentRepository  extends JpaRepository<Comment,Long>{
-
+	 Page<Comment> findByPostId(Long postId, Pageable pageable);
+	 Optional<Comment> findByIdAndPostId(Long id, Long postId);
 }
