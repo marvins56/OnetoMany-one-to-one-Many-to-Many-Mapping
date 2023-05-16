079197e5decbee201ce2cb4795b113c6580e6ea0
Kauta Marvin <82571414+marvins56@users.noreply.github.com>
2023-05-16 21:17:04 +0300

    😏 Comment Model

---
+++ src/main/java/com/example/OnetoManyMapping/model/Comment.java
@@ -0,0 +1,73 @@
+package com.example.OnetoManyMapping.model;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "comments")
+public class Comment extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Lob
+    private String text;
+    
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "post_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Post post;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Post getPost() {
+		return post;
+	}
+
+	public void setPost(Post post) {
+		this.post = post;
+	}
+
+	public Comment(@NotNull String text, Post post) {
+		
+		this.text = text;
+		this.post = post;
+	}
+
+	public Comment() {
+		super();
+	}
+    
+    
+    
+    
+}
---
+++ src/main/java/com/example/OnetoManyMapping/model/Post.java
@@ -0,0 +1,80 @@
+package com.example.OnetoManyMapping.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "posts")
+public class Post extends AuditModel{
+
+	  @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+
+	    @NotNull
+	    @Size(max = 100)
+	    @Column(unique = true)
+	    private String title;
+
+	    @NotNull
+	    @Size(max = 250)
+	    private String description;
+
+	    @NotNull
+	    @Lob
+	    private String content;
+	    
+		public Post() {
+			super();
+		}
+
+		public Post(@NotNull @Size(max = 100) String title, @NotNull @Size(max = 250) String description,
+				@NotNull String content) {
+		
+			this.title = title;
+			this.description = description;
+			this.content = content;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getTitle() {
+			return title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public String getContent() {
+			return content;
+		}
+
+		public void setContent(String content) {
+			this.content = content;
+		}
+	
+	    
+	    
+}
